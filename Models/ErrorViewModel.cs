97c56e3dfac899453ed8691e18bc1e077d121189
giahuy <boomhuyxt@gmail.com>
2026-03-29 21:43:13 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Web_quan_ly_nhan_su.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
