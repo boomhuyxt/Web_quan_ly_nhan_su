11f34e551e96d41b78abad805bafabc49806e048
giahuy <boomhuyxt@gmail.com>
2026-05-25 01:05:18 +0700

    sữa lỗi gửi file

--- Controllers/AiController.cs
+++ Controllers/AiController.cs
@@ -104,7 +104,7 @@ namespace Web_quan_ly_nhan_su.Controllers
             var danhSachFileMau = new List<(string TuKhoa, string TenFile, string Url, string Icon)>
             {
                 ("nghỉ việc", "Mẫu đơn xin nghỉ phép.docx", "https://dwdvizkleazjodyfbovl.supabase.co/storage/v1/object/public/FileMau/Mau_Don_Xin_Nghi_Phep.doc", "description"),
-                ("thôi việc", "Mẫu đơn xin thôi việc.docx", "https://dwdvizkleazjodyfbovl.supabase.co/storage/v1/object/public/FileMau/Don_Xin_Thoi_Viec.docx", "description"),
+                ("thôi việc", "Mẫu đơn xin thôi việc.docx", "https://dwdvizkleazjodyfbovl.supabase.co/storage/v1/object/public/FileMau/Mau_Don_Xin_Nghi_Phep.doc", "description"),
                 ("nghỉ việt", "Mẫu đơn xin nghỉ phép.docx", "https://dwdvizkleazjodyfbovl.supabase.co/storage/v1/object/public/FileMau/Mau_Don_Xin_Nghi_Phep.doc", "description"),
                 ("don nghi", "Mẫu đơn xin nghỉ phép.docx", "https://dwdvizkleazjodyfbovl.supabase.co/storage/v1/object/public/FileMau/Mau_Don_Xin_Nghi_Phep.doc", "description"),
                 ("hợp đồng", "Mẫu hợp đồng lao động tiêu chuẩn.docx", "https://dwdvizkleazjodyfbovl.supabase.co/storage/v1/object/public/FileMau/Mau_hop_dong_lao_dong_tieu_chuan.docx", "description"),
